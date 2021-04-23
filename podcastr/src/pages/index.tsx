a31d766deddedaec61abbf789ed933b23cbfcb5f
Daniela C. Barbosa <contato@danielacb.com>
2021-04-23 01:07:06 -0300

    add api data

--- podcastr/src/pages/index.tsx
+++ podcastr/src/pages/index.tsx
@@ -1,5 +1,60 @@
-import Header from "../components/Header";
+import { GetStaticProps } from "next";
+import { format, parseISO } from "date-fns";
+import { ptBR } from "date-fns/locale";
+import { api } from "../services/api";
+import { convertDurationToTimeString } from "../utils/convertDurationToTimeString";
 
-export default function Home() {
-  return <h1>Home</h1>;
+type Episodes = {
+  id: string;
+  title: string;
+  members: string;
+  thumbnail: string;
+  publishedAt: string;
+  duration: number;
+  durationAsString: string;
+  description: string;
+  url: string;
+};
+
+type HomeProps = {
+  episodes: Episodes[];
+};
+
+export default function Home(props: HomeProps) {
+  return <p>{JSON.stringify(props)}</p>;
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const { data } = await api.get("episodes", {
+    params: {
+      _limit: 12,
+      _sort: "published_at",
+      _order: "desc",
+    },
+  });
+
+  const episodes = data.map((episode) => {
+    return {
+      id: episode.id,
+      title: episode.title,
+      members: episode.members,
+      thumbnail: episode.thumbnail,
+      publishedAt: format(parseISO(episode.published_at), "d MMM yy", {
+        locale: ptBR,
+      }),
+      duration: Number(episode.file.duration),
+      durationAsString: convertDurationToTimeString(
+        Number(episode.file.duration)
+      ),
+      description: episode.description,
+      url: episode.file.url,
+    };
+  });
+
+  return {
+    props: {
+      episodes: episodes,
+    },
+    revalidate: 60 * 60 * 8,
+  };
+};
