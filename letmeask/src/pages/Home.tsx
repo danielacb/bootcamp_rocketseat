070b79a165b43e55abbeea934e43ef95ffb9ede0
Daniela C. Barbosa <contato@danielacb.com>
2021-06-24 22:21:50 -0300

    create Home page

---
+++ letmeask/src/pages/Home.tsx
@@ -0,0 +1,36 @@
+import illustrationImg from "../assets/images/illustration.svg";
+import logoImg from "../assets/images/logo.svg";
+import googleIconImg from "../assets/images/google-icon.svg";
+
+import { Button } from "../components/Button";
+
+import "../styles/auth.scss";
+
+export function Home() {
+  return (
+    <div id="page-auth">
+      <aside>
+        <img
+          src={illustrationImg}
+          alt="Ilustração simbolizando perguntas e respostas"
+        />
+        <strong>Toda pergunta tem uma resposta.</strong>
+        <p>Aprenda e compartilhe conhecimento com outras pessoas</p>
+      </aside>
+      <main>
+        <div className="main-content">
+          <img src={logoImg} alt="Logo escrita Letmeask" />
+          <button className="create-room">
+            <img src={googleIconImg} alt="Logo do Google" />
+            Crie sua sala com o Google
+          </button>
+          <span className="separator">ou entre em uma sala</span>
+          <form action="">
+            <input type="text" placeholder="Digite o código da sala" />
+            <Button type="submit">Entrar na sala</Button>
+          </form>
+        </div>
+      </main>
+    </div>
+  );
+}
