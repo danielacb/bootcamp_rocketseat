28c452661f32449ef0785fca6fa8aa9018bba55c
Daniela Barbosa <contato@danielacb.com>
2022-01-01 16:43:12 -0300

    create Header component

---
+++ react-native/plantmanager/src/components/Header.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { Image, StyleSheet, Text, View } from "react-native";
+import { getStatusBarHeight } from "react-native-iphone-x-helper";
+
+import ProfileImage from "../assets/profile.png";
+import colors from "../styles/colors";
+import fonts from "../styles/fonts";
+
+export default function Header() {
+  return (
+    <View style={styles.container}>
+      <View>
+        <Text style={styles.greeting}>Hi,</Text>
+        <Text style={styles.userName}>Nome</Text>
+      </View>
+      <Image source={ProfileImage} style={styles.image} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingVertical: 20,
+    marginTop: getStatusBarHeight(),
+  },
+  greeting: {
+    fontSize: 32,
+    color: colors.heading,
+    fontFamily: fonts.text,
+  },
+  userName: {
+    fontSize: 32,
+    color: colors.heading,
+    fontFamily: fonts.heading,
+    lineHeight: 36,
+  },
+  image: {
+    borderRadius: 40,
+    width: 56,
+    height: 56,
+  },
+});
